ce064592725328a03530c2033f50bb7c09ad9cec
Leilya <ahmetshina.leyla@mail.ru>
2023-08-04 01:04:51 +0300

    Дз lvl_2.4

---
+++ lvl_2/task_2.4.py
@@ -0,0 +1,49 @@
+# Задача 2.4.
+
+# Пункт A.
+# Напишите функцию, которая удаляет все восклицательные знаки из заданной строк.
+# Например,
+# foo("Hi! Hello!") -> "Hi Hello"
+# foo("") -> ""
+# foo("Oh, no!!!") -> "Oh, no"
+
+def remove_exclamation_marks(s):
+    st = s.replace('!', '') 
+    return st 
+
+stroka = "Hi! Hello!"
+
+print(f'{remove_exclamation_marks (stroka)}')
+
+
+# Пункт B.
+# Удалите восклицательный знак из конца строки. 
+# remove("Hi!") == "Hi"
+# remove("Hi!!!") == "Hi!!"
+# remove("!Hi") == "!Hi"
+
+def remove_last_em(s):
+    if s[-1] == '!':
+        return s[:-1]
+    else:
+        return(s)
+
+stroka = "Hi!!!"
+
+print(f'{remove_last_em (stroka)}')
+
+
+# Дополнительно
+
+# Пункт С.
+# Удалите слова из предложения, если они содержат ровно один восклицательный знак.
+# Слова разделены одним пробелом.
+# Например,
+# remove("Hi!") === ""
+# remove("Hi! Hi!") === ""
+# remove("Hi! Hi! Hi!") === ""
+# remove("Hi Hi! Hi!") === "Hi"
+# remove("Hi! !Hi Hi!") === ""
+# remove("Hi! Hi!! Hi!") === "Hi!!"
+# remove("Hi! !Hi! Hi!") === "!Hi!"
+
